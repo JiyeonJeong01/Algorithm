a2c3ac10f52e211c3dfa0153a62a8316105c64ce
JiyeonJeong01 <jizzvibe@gmail.com>
2025-07-04 20:12:49 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -1,24 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-int N;
+int main(void)
+{
+	int n; cin >> n;
 
-int main(void) {
-    ios::sync_with_stdio(0); cin.tie(0);
-    cin >> N;
-    int two = 0, five = 0;
-    for (int i = 2; i <= N; i++) {
-        int num = i;
-        while (num % 2 == 0){
-            num /= 2;
-            two++;
-        }
-        while (num % 5 == 0){
-            num /= 5;
-            five++;
-        }
-    }
-    // cout << two << " " << five <<"\n";
-    int mn = min(two, five);     
-    cout << mn;        
+	int res = (n / 5) + (n / 25) + (n / 125);
+	cout << res;
+
+	return 0;
 }
