646747cae8dc821811be9b47ff2a70ba659bb932
JiyeonJeong01 <jizzvibe@gmail.com>
2025-07-12 12:52:59 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{   
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x1, x2, x3, y1, y2, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 ;
+
+    int result = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+
+    if (result == 0)
+    {
+        cout << 0;
+    }
+    else if (result > 0)
+    {
+        cout << 1;
+    }
+    else 
+    {
+        cout << -1;
+    }
+
+}
+
