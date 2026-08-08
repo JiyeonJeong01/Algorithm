c74d858e5bb308234ced00ab0ed06d1f113752c1
JiyeonJeong01 <jizzvibe@gmail.com>
2026-08-08 19:27:59 +0900

    [level 2] Title: 프로세스, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -4,51 +4,34 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-
-    // 우선순위, 인덱스
-    queue<pair<int, int>> q;
-
+    
+    queue<int> cq;
+    vector<int> sorted;
+    
     for (int i = 0; i < priorities.size(); ++i)
+        cq.push(i);
+    
+    while(!cq.empty())
     {
-        q.push({ priorities[i], i });
-    }
-
-    while (!q.empty())
-    {
-        auto curr = q.front();
-        q.pop();
+        int currIdx = cq.front();
+        cq.pop();
         
-        // 1. q 안에 curr보다 우선순위 높은 것이 있는지 찾기
-        bool hasHigher = false;
-        int qSize = q.size();
-        for (int i = 0; i < qSize; ++i)
+        if (priorities[currIdx] 
+           != *(max_element(priorities.begin(), priorities.end())))
         {
-            auto temp = q.front();
-            q.pop();
-            
-            if (temp.first > curr.first)
-                hasHigher = true;
-            
-            q.push(temp);
+            cq.push(currIdx);
         }
-
-        // 2. 있다면 curr를 q 뒤에 다시 넣기
-        if (hasHigher)
-        {
-            q.push(curr);
-        }
-
-        // 3. 없다면 실행 처리
         else 
         {
+            // 프로세스 실행
             answer++;
-            if (location == curr.second)
+            priorities[currIdx] = 0;
+            
+            if (currIdx == location)
                 return answer;
         }
-
-        // 4. 실행된 프로세스의 index가 location이면 return
-
     }
+
     
     return answer;
 }
