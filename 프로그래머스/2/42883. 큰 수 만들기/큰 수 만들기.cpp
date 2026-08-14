7e53dcd9745547299853443fd95fd9888e938eaa
JiyeonJeong01 <jizzvibe@gmail.com>
2026-08-14 14:57:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 2.90 ms, Memory: 5.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string number, int k) {
+
+    string answer = "";
+    
+    for (int i = 0; i < number.size(); ++i)
+    {
+        while (!answer.empty() 
+           && k > 0
+           && answer.back() < number[i])
+        {
+            answer.pop_back();
+            k--;
+        }
+        answer.push_back(number[i]);
+    }
+    
+    while (k-- > 0)
+    {
+        answer.pop_back();
+    }
+    
+    return answer;
+}
