b72b7e7733016378f1615c605d3898c16c1f3828
JiyeonJeong01 <jizzvibe@gmail.com>
2026-08-18 14:48:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.09 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,55 +1,55 @@
 #include <bits/stdc++.h>
 
+#define INT_MAX_V 99999
+
 using namespace std;
 
-const int MAX_INT = 99999;
-    
 vector<vector<int>> gWires;
-vector<bool> gVisited;
 
-int DFS(int cur, int cutA, int cutB)
+int Search(
+    vector<bool>& visited, 
+    const pair<int, int>& cut, 
+    int cur)
 {
-    gVisited[cur] = true;
-    
+    visited[cur] = true;
     int cnt = 1;
-    for (int next : gWires[cur])
+    // 내 노드와 연결된 노드 전부 방문 시도
+    for (auto next : gWires[cur])
     {
-        if (gVisited[next])
+        // 이미 방문했으면 방문하지 않는다.
+        if (visited[next])
             continue;
         
-        // 끊긴 간선, 양방향 고려
-        if ((cur == cutA && next == cutB)
-           || (cur == cutB && next == cutA))
+        if ((cut.first == cur && cut.second == next)
+           || cut.first == next && cut.second == cur)
             continue;
         
-        cnt += DFS(next, cutA, cutB);
+        cnt += Search(visited, cut, next);
     }
     
+    // 현재까지의 깊이를 반환한다.
     return cnt;
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    
+
     gWires = vector<vector<int>>(n + 1);
     
-    // 인접리스트 만들기
-    for (const auto& wire : wires)
-    {
-        gWires[wire[0]].push_back(wire[1]);
-        gWires[wire[1]].push_back(wire[0]);
-    }
-    
-    int mn = MAX_INT;
-    
-    // wires[0]부터 전부 끊어보면서 최소 diff 찾기
     for (int i = 0; i < wires.size(); ++i)
     {
-        gVisited = vector<bool>(n + 1, false);
-        
-        int area = DFS(1, wires[i][0], wires[i][1]);
+        gWires[wires[i][0]].push_back(wires[i][1]);
+        gWires[wires[i][1]].push_back(wires[i][0]);
+    }
+    
+    // 전선을 하나씩 끊어보며 전부 확인해야 한다.
+    int mn = INT_MAX_V;
+    for (int i = 0; i < wires.size(); ++i)
+    {
+        vector<bool> visited(n + 1, false);
+        // 끊는다.
+        int area = Search(visited, {wires[i][0], wires[i][1]}, 1);
         
         int diff = abs(2 * area - n);
-        
         mn = min(diff, mn);
     }
     
