b234fa9a3603afd096710c83b5a0aa5c36d4e6fc
JiyeonJeong01 <jizzvibe@gmail.com>
2026-09-01 10:16:42 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 25.91 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,83 +1,84 @@
 #include <bits/stdc++.h>
 
+#define EMPTY 0
+#define WALL 1
+
 using namespace std;
 using Cell = pair<int, int>;
 
-const int EMPTY = 0;
-const int WALL = 1;
-
-const int dr[4] = { 1, -1, 0, 0 };
-const int dc[4] = { 0, 0, -1, 1 };
-    
 const int INF = numeric_limits<int>::max();
+const int dr[4] = { -1, 1, 0, 0 };
+const int dc[4] = { 0, 0, -1, 1 };
 
-vector<vector<int>> board;
-vector<vector<vector<int>>> minCosts;
-Cell goal;
-int boardSize;
+int gSize;
+Cell gGoal;
+vector<vector<int>> gBoard;
+vector<vector<vector<int>>> gMinCost;   // row, col, prevDir
 
-void DFS(
+void Visit(
     const int prevDir,
+    const Cell& curCell,
     const int curCost,
-    const Cell cur,
     int& minCost)
 {
-    if (cur == goal)
+    // 현재 경로의 종료 조건.
+    if (curCell == gGoal)
     {
-        minCost = min(curCost, minCost);
+        minCost = min(minCost, curCost);
         return;
     }
-    
-    for (int i = 0; i < 4; ++i)
-    {
-        int nrow = cur.first + dr[i];
-        int ncol = cur.second + dc[i];
         
-        if (nrow < 0 || ncol < 0 || nrow >= boardSize || ncol >= boardSize)
+    // 네 방향 탐색
+    for (int dir = 0; dir < 4; ++dir)
+    {
+        int nrow = curCell.first + dr[dir];
+        int ncol = curCell.second + dc[dir];
+        
+        if (nrow < 0 || ncol < 0 || nrow >= gSize || ncol >= gSize)
             continue;
         
-        if (board[nrow][ncol] == WALL)
+        if (gBoard[nrow][ncol] == WALL)
             continue;
         
         int nextCost = curCost + 100;
-        if (i != prevDir)
-            nextCost += 500;
+        if (prevDir != dir)
+            nextCost += 500; // 코너 비용
         
-        if (minCosts[nrow][ncol][i] <= nextCost || nextCost >= minCost)
+        if (gMinCost[nrow][ncol][dir] <= nextCost)
             continue;
         
-        minCosts[nrow][ncol][i] = nextCost;
-        DFS(i, nextCost, {nrow, ncol}, minCost);
-    }
+        gMinCost[nrow][ncol][dir] = nextCost;
+        Visit(dir, {nrow, ncol}, nextCost, minCost);
+    }   
 }
 
-int solution(vector<vector<int>> b)
-{
-    board = b;
-    boardSize = board.size();
-    
-    // 3차원 벡터. row, cell, prevDir
-    minCosts = vector<vector<vector<int>>> (
-        boardSize,
-        vector<vector<int>>(boardSize, vector<int>(4, INF))
-    );
-    goal = { boardSize - 1, boardSize - 1 };
+int solution(vector<vector<int>> board)
+{   
+    gSize = board.size();
+    gGoal = { gSize - 1, gSize - 1 };
+    gBoard = board;
+    gMinCost = vector<vector<vector<int>>>(
+            gSize, 
+            vector<vector<int>>(
+                gSize, vector<int>(4, INF)));
     
     int minCost = INF;
     
-    for (int i = 0; i < 4; ++i)
+    for (int dir = 0; dir < 4; ++dir)
     {
-        int nrow = dr[i];
-        int ncol = dc[i];
+        int nrow = dr[dir];
+        int ncol = dc[dir];
         
-        if (nrow < 0 || ncol < 0 || nrow >= boardSize || ncol >= boardSize)
+        if (nrow < 0 || ncol < 0 || nrow >= gSize || ncol >= gSize)
             continue;
         
-        if (board[nrow][ncol] == WALL)
+        if (gBoard[nrow][ncol] == WALL)
             continue;
-        minCosts[nrow][ncol][i] = 100;
-        DFS(i, 100, {nrow, ncol}, minCost);
-    }
-
+        
+        int nextCost = 100;
+        gMinCost[nrow][ncol][dir] = nextCost;
+        Visit(dir, {nrow, ncol}, nextCost, minCost);
+    }  
+    
     return minCost;
 }
